5fa714bc9e7d7d71734b82e4c7faeac26a4449a2
TOBAIS123 <erfanyeganehfar@gmail.com>
2022-01-17 22:57:08 -0500

    stuf

--- Lab1/lab1.c
+++ Lab1/lab1.c
@@ -1,6 +1,64 @@
 #include<stdio.h>
-
+#include<math.h>
+#include<string.h>
 
 double split_bill(double base_amount , double tax_rate , double tip_rate , int num_people) {
-// Your code goes here ...
+    double total_split=(base_amount*(1.0+tax_rate))*(1.0+tip_rate)/num_people;
+    //return total_split+0.005;
+    if (((int)total_split*1000)%10 != 0){
+        total_split = total_split+0.005;
+    }
+    return total_split;
+
+}
+
+double adjust_price(double original_price) {
+    return 10*pow(original_price,0.5);
+}
+
+
+int sandy_eats(char menu_item []){
+    
+    int flag;
+
+    //convert all uppercase chars to lowercase
+    for(int i = 0; menu_item[i]; i++){
+         if (menu_item[i] >= 65 && menu_item[i] <= 90) {
+            menu_item[i] = menu_item[i] + 32;
+        }  
+    }
+    
+    int count = 0;  
+    
+    //Counts each char  except space  
+    for(int i = 0; i < strlen(menu_item); i++) {  
+        if(menu_item[i] != ' ')  
+            count++;  
+    }
+
+    if (count%2==1){
+        flag=0;
+    }
+
+    char flag_words[4][5]= {"j","k","l","fish"};
+
+    for (int j=0; j<4; j++){
+        if (strstr(menu_item, flag_words[j]) != NULL){
+            flag=0;
+        }
+    }
+
+    return 0;
+}
+
+
+void imagine_fish(char thing[]){
+    int str_len= strlen(thing);
+    char word[]= "fish";
+
+    for (int i=0; i<5; i++){
+        thing[str_len+i]=word[i];
+    }
+
+
 }
--- Lab1/test1.c
+++ Lab1/test1.c
@@ -1,6 +1,6 @@
 #include <stdio.h> //need this to use the printf () function
 #include "lab1.h" //need this to link our Lab 1 functions
-
+#include "lab1.c"
 
 int main() {
     // Testing Part 1
@@ -8,5 +8,30 @@ int main() {
     printf("The split will be: $%.2f.\n", test_p1 );
     // $32 .50
     //Add more tests here ...
+
+    // Testing Part 2
+    double test_p2 = adjust_price (25.00);
+    printf("The adjusted price is: $%.2f.\n", test_p2 );
+    // Should be $50 .00
+
+
+    // Testing Part 3
+    char test_food [] = "Three Cheese Pizza";
+    int test_p3 = sandy_eats(test_food );
+    if (test_p3 == 0){
+    printf("Sandy would NOT eat ‘%s ’.\n", test_food );
+    }
+    else{
+    printf("Sandy would eat ‘%s ’.\n", test_food );
+    }
+    // Sandy would eat ‘Three Cheese Pizza ’.
+
+
+    // Testing Part 4
+    char test_p4 [30] = "zebra";
+    imagine_fish(test_p4 );
+    printf("Just imagine , a %s!\n", test_p4 );
+    // Just imagine , a zebrafish!
+
     return 0;
 }
