b62c055881bb6c2238ff229659ef0cd8af692022
TOBAIS123 <erfanyeganehfar@gmail.com>
2022-04-12 10:40:26 -0400

    update

--- L5_starter-code/lab5.c
+++ L5_starter-code/lab5.c
@@ -67,56 +67,10 @@ void disruptEdge(Vnode *node, char *station)
     }
 }
 
-/*
 char **plan_route(Graph *gr, char *start, char *dest)
 {
-
-    // Add code here
-    int cost[MAX][MAX], distance[MAX], pred[MAX];
-    int visited[MAX], count, mindistance, nextnode, i, j;
-    // pred[] stores the predecessor of each node
-    // count gives the number of nodes seen so far
-    // create the cost matrix
-    for (i = 0; i < n; i++)
-        for (j = 0; j < n; j++)
-            if (G[i][j] == 0)
-                cost[i][j] = INFINITY;
-            else
-                cost[i][j] = G[i][j];
-    // initialize pred[],distance[] and visited[]
-    for (i = 0; i < n; i++)
-    {
-        distance[i] = cost[startnode][i];
-        pred[i] = startnode;
-        visited[i] = 0;
-    }
-    distance[startnode] = 0;
-    visited[startnode] = 1;
-    count = 1;
-    while (count < n - 1)
-    {
-        mindistance = INFINITY;
-        // nextnode gives the node at minimum distance
-        for (i = 0; i < n; i++)
-            if (distance[i] < mindistance && !visited[i])
-            {
-                mindistance = distance[i];
-                nextnode = i;
-            }
-        // check if a better path exists through nextnode
-        visited[nextnode] = 1;
-        for (i = 0; i < n; i++)
-            if (!visited[i])
-                if (mindistance + cost[nextnode][i] < distance[i])
-                {
-                    distance[i] = mindistance + cost[nextnode][i];
-                    pred[i] = nextnode;
-                }
-        count++;
-    }
-    return;
 }
-*/
+
 void add(Graph *gr, char *station)
 {
     for (int i = 0; i < gr->count; i++)
